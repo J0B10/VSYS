3b5d5c6d96f5d9d656bfb0ff8bf03f49e1f27a96
Stefan Traub <traub@hs-ulm.de>
2022-12-22 11:15:57 +0100

    Aufhgabe 6

--- uebung/wwwroot/js/site.js
+++ uebung/wwwroot/js/site.js
@@ -1,6 +1,11 @@
-﻿$(async ()=>{
-    let r = await $.get("/Time")
-    $("#time").html(r.time)
-    let s = await $.get("/Message")
-    $("#message").html(s.message)
+﻿$(()=>{
+    window.setInterval(async ()=>{
+        let r = await $.get("/Time")
+        $("#time").html(r.time)
+    }, 1000)
+    $("#nachricht").click(async ()=>{
+        console.log("click")
+        let s = await $.get("/Message")
+        $("#message").html(s.message)
+    })
 })
